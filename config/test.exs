73624fc98474f806f926118d77a7737486da1bac
ema <emanuele@codiceplastico.com>
2020-11-04 16:58:03 +0100

    fix connection string

--- config/test.exs
+++ config/test.exs
@@ -4,5 +4,5 @@ config :todo_api, TodoApi.Repo,
   database: "todo_db_test",
   username: "postgres",
   password: "postgres",
-  hostname: "localhost",
+  hostname: "postgres",
   pool: Ecto.Adapters.SQL.Sandbox
